2f93ab16bc90a477f4ccbad21fc433026ef2350d
LasyaGanesuni <66461307+LasyaGanesuni@users.noreply.github.com>
2022-03-06 16:06:22 +0530

    files uploaded

---
+++ Login files/Controllers/UserController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_Role_MVC.Models;
+
+namespace Login_Role_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult GetUserData()
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+
+            LoginVM model = new LoginVM();
+            model.LoginLst = dal.Display();
+
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddNewUser()
+        {
+            AddUserVM model = new AddUserVM();
+            model.RoleLst = GetRoleData();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult AddNewUser(AddUserVM model)
+        {
+            UserVM ur = new UserVM();
+            ur.Id = model.Id;
+            ur.Name = model.Name;
+            ur.UserId = model.UserId;
+            ur.Password = model.Password;
+            ur.RoleId = model.RoleId;
+            
+            DataAccessLayer dal = new DataAccessLayer();
+            model.RoleLst = GetRoleData();
+            bool flag = dal.AddUser(ur);
+
+            if(flag)
+            {
+                ViewBag.Msg = "Details Added Successfully";
+            }
+            else
+            {
+                ViewBag.Msg = "Error Occurred while adding";
+            }
+            
+            return View(model);
+        }
+        public List<SelectListItem> GetRoleData()
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+
+            List<SelectListItem> Rlst = new List<SelectListItem>();
+
+            List<RoleVM> lst = dal.GetRole();
+
+            foreach(var r in lst)
+            {
+                Rlst.Add(new SelectListItem() { Text = r.RoleName, Value = r.RoleId.ToString() });
+            }
+            return Rlst;
+        }
+        [HttpGet]
+        public ActionResult DeleteData(long id)
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+            dal.DeleteRow(id);
+            return RedirectToAction("GetUserData");
+        }
+        [HttpGet]
+        public ActionResult EditData(long id)
+        {
+            AddUserVM model = new AddUserVM();
+
+            DataAccessLayer dal = new DataAccessLayer();
+            UserVM obj = dal.ShowUserDetails(id);
+            if (obj != null)
+            {
+                model.Id = obj.Id;
+                model.Name = obj.Name;
+                model.UserId = obj.UserId;
+                model.Password = obj.Password;
+                model.RoleId = obj.RoleId;
+
+                model.RoleLst = GetRoleData();
+
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("GetUserData");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult EditData(AddUserVM model)
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+            UserVM obj = new UserVM();
+            obj.Id = model.Id;
+            obj.Name = model.Name;
+            obj.UserId = model.UserId;
+            obj.Password = model.Password;
+            obj.RoleId = model.RoleId;
+            dal.UpdateUser(obj);
+            return RedirectToAction("GetUserData");
+        }
+    }
+}
---
+++ Login files/Models/UserVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+namespace Login_Role_MVC.Models
+{
+    public class UserVM
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public long RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+    public class LoginVM
+    {
+        public List<UserVM> LoginLst { get; set; } = new List<UserVM>();
+    }
+
+    public class AddUserVM
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public long RoleId { get; set; }
+        public List<SelectListItem> RoleLst { get; set; } = new List<SelectListItem>();
+    }
+    public class RoleVM
+    {
+        public long RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
